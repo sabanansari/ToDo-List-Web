e1bde31dabf31b97fef8c3d9816351ec2870e3d2
sabanansari <alisaban52@gmail.com>
2023-02-12 18:11:51 +0530

    changed connection to mongodb Atlas and deploying to Heroku

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-mongoose.connect('mongodb://127.0.0.1:27017/todolistDB');
+mongoose.connect('mongodb+srv://admin-ansari:Test123@cluster0.blrmget.mongodb.net/todolistDB');
 
 const itemsSchema = {name: String};
 const Item = mongoose.model("Item",itemsSchema);
